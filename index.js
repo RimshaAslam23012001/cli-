91d4be1a20bb8e50d64f1a458f0484800437c0e3
Rimsha Aslam <rimsha.aslam.230@gmail.com>
2024-04-27 00:42:27 +0500

    Adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twelcome to cli-number-guessing-game\n");
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 8):",
+    },
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulation ! You guess a correct number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
